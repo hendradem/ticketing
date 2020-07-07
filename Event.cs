30504f489f8b8ed4595f22f7f3ee80899c80ce3f
Ade Mahendra <hendraadem@gmail.com>
2020-07-07 19:45:26 +0700

    initial commit

---
+++ Event.cs
@@ -0,0 +1,17 @@
+namespace c_example{
+    public class Event{
+
+        private string eventName;  
+
+        public re(string avalue) {  
+            eventName = avalue;  
+        }  
+
+        public string Departname {  
+            get {  
+                return departname;  
+            }  
+        }  
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace c_example{
+    class Program{
+        static void Main(string[] args){
+            bool showMenu = true;
+            while (showMenu){
+                showMenu = MainMenu();
+            }
+        }
+
+        private static bool MainMenu(){
+            Console.Clear();
+            Console.WriteLine(" _________________________________");
+            Console.WriteLine("|                                 |");
+            Console.WriteLine("|                                 |");
+            Console.WriteLine("|             WELCOME             |");
+            Console.WriteLine("|          Ticketing App          |");
+            Console.WriteLine("|                                 |");
+            Console.WriteLine("|_________________________________|");
+            Console.WriteLine("|                                 |");
+            Console.WriteLine("| 1.HOME |  2.BOOK | 3.EVENT LIST |");
+            Console.WriteLine("|_________________________________|");
+            Console.Write("\r\nSelect an option: ");
+ 
+            switch (Console.ReadLine()){
+                case "1":
+                    ReverseString();
+                    return true;
+                case "2":
+                    RemoveWhitespace();
+                    return true;
+                case "3":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        
+ 
+        private static string CaptureInput(){
+            Console.Write("Enter the string you want to modify: ");
+            return Console.ReadLine();
+        }
+ 
+        private static void ReverseString(){
+            Console.Clear();
+            Console.WriteLine("Reverse String");
+ 
+            char[] charArray = CaptureInput().ToCharArray();
+            Array.Reverse(charArray);
+            DisplayResult(String.Concat(charArray));
+        }
+ 
+        private static void RemoveWhitespace(){
+            Console.Clear();
+            Console.WriteLine("Remove Whitespace");
+ 
+            DisplayResult(CaptureInput().Replace(" ", ""));
+        }
+ 
+        private static void DisplayResult(string message){
+            Console.WriteLine($"\r\nYour modified string is: {message}");
+            Console.Write("\r\nPress Enter to return to Main Menu");
+            Console.ReadLine();
+        }
+    }
+}
