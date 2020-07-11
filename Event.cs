7c4f4dfe276a3aee4d6e559d29b9315f262d0913
Ade Mahendra <hendraadem@gmail.com>
2020-07-11 22:28:33 +0700

    updating event list

--- Event.cs
+++ Event.cs
@@ -1,42 +1,29 @@
+using System;
 namespace c_example{
-    // public class Event{
+    
+    class Event{
+        private string JenisEvent;
 
-    //     private string eventName;  
+        public void dataevent (string JenisEvent){
+            string[,] Namaevent = new string[,]{
+                {"kondangan" , "50.000" },
+                {"makan-makan", "30.000" },
+                {"reuni","20.000" },
+                {"wisuda","100.000" },
+                {"coronavirus", "unlimited" }
+            };
 
-    //     public re(string avalue) {  
-    //         eventName = avalue;  
-    //     }  
+            for(int i = 0; i <= Namaevent.GetUpperBound(0); i++){
+                string Kegiatan = Namaevent[i, 0];
+                string HTM = Namaevent[i, 1];
+                Console.WriteLine("{0} {1}", Kegiatan,HTM);
+            }
 
-    //     public string Departname {  
-    //         get {  
-    //             return departname;  
-    //         }  
-    //     }  
-
-    // }
-
-
-     public class Tabungan {
-		
-        private double saldo = 0;
-        // array (data event [nama event, tgl, harga tiket])
-        
-
-        // Getter & Setter
-        public void Menabung(double n) {
-            saldo += n;
+            Console.ReadLine();
         }
-        public void Mengambil(double n) {
-            saldo -= n;
-        }
-
-
-        public double GetSaldo() {
-            return saldo;
+        public string getJenisEvent(){
+            return this.JenisEvent;
         }
     }
-
-   
-
 
 }
