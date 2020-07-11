93eb4865776fff47d22b0fd7d911de85104c7ecc
Ade Mahendra <hendraadem@gmail.com>
2020-07-11 23:59:44 +0700

    change bookEvent method functionality

--- bookEvent.cs
+++ bookEvent.cs
@@ -1,7 +1,25 @@
-namespace ticketing
-{
-    public class bookEvent
-    {
-        
+using System;
+using System.Collections; 
+using System.Collections.Generic; 
+
+namespace ticketing{
+    class bookEvent{
+
+        private string eventName, eventPrice;
+
+        public string getEventName(){
+            return eventName;
+        }
+        public void setEventName(string eName){
+            eventName = eName;
+        }
+
+        public string getEventPrice(){
+            return eventName;
+        }
+        public void setEventPrice(string ePrice){
+            eventName = ePrice;
+        }
+
     }
 }
