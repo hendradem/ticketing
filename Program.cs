9f3c016d8436bfacc2a2421877ccbd953cd8d78f
Ade Mahendra <hendraadem@gmail.com>
2020-07-12 13:59:02 +0700

    fixing

--- Program.cs
+++ Program.cs
@@ -1,25 +1,118 @@
 ﻿using System;
 
-namespace c_example{
+namespace ticketing{
     class Program{
         static void Main(string[] args){
-            bool showMenu = true;
-            while (showMenu){
-                showMenu = MainMenu();
+            // bool showMenu = true;
+            // while (showMenu){
+            //     showMenu = MainMenu();
+            // }
+
+            Console.Clear();
+            Console.WriteLine(" _________________________________");
+            Console.WriteLine("|                                 |");
+            Console.WriteLine("|                                 |");
+            Console.WriteLine("|             WELCOME             |");
+            Console.WriteLine("|          Ticketing App          |");
+            Console.WriteLine("|                                 |");
+            Console.WriteLine("|_________________________________|");
+            Console.WriteLine("|                                 |");
+            Console.WriteLine("| 1.HOME |  2.BOOK | 3.EVENT LIST |");
+            Console.WriteLine("|_________________________________|");
+
+            Console.Write("\r\nSelect an option: ");
+
+            int men = 0;
+            men = Convert.ToInt16( Console.ReadLine() );
+ 
+            switch (men){
+                case 1 :
+                    ReverseString();
+                    break;
+                case 2:
+                    bookEvents();
+                    break;
+                case 3:
+                    eventList();
+                    break;
+                default :
+                    Console.WriteLine("akljdlkjasdjk");
+                    break;
             }
+        }
+
+        private static void bookEvents(){
+            // bookEvent[] booking = new bookEvent[1];
+            // string eName, ePrice;
+
+            // for(int i=0; i<booking.Length; i++){
+
+            //     booking[i] = new bookEvent();
+
+            //     Console.WriteLine("Masukkan nama event : ");
+            //     eName = Console.ReadLine();
+
+            //     Console.WriteLine("Masukkan harga tiket : ");
+            //     ePrice = Console.ReadLine();
+
+            //     booking[i].setEvent(eName, ePrice);
+
+            //     // Console.WriteLine("================");
+            //     // Console.WriteLine(booking[i].getEvent());
+
+            // }
+
+            // booking.getEvent();
+            // Console.ReadLine();
+
+
+
+            Console.WriteLine("Masukkan jumlah");
+            int size = Convert.ToInt32( Console.In.ReadLine() );
+            string name, price, cust, phone;
+            string[] mhs = new string[size];
+
+            bookEvent e = new bookEvent();
+
+            for(int i=0; i<mhs.Length; i++){
+                Console.Write("Nama Event : ");
+                name = Console.ReadLine();
+
+                Console.Write("Harga Tiket : ");
+                price = Console.ReadLine();
+
+                Console.Write("Nama Pemesan : ");
+                cust = Console.ReadLine();
+
+                Console.Write("No HP Pemesan : ");
+                phone = Console.ReadLine();
+
+                e.setEvent(name, price, cust, phone);
+            }
+
+            e.getEvent();
+            Console.ReadLine();
 
 
         }
 
-         private static void eventList(){
+        
+        public static void backToMenu(){
+            Console.WriteLine("Press 1 to back to menu : ");
+            string men = Console.ReadLine();
+            if(men == "1"){
+                // Main();
+            }
+        }
 
+        public static void eventList(){
+            Console.WriteLine("|---------------|------------|");
+            Console.WriteLine("|-----EVENT-----|----HARGA---|");
+            Console.WriteLine("|---------------|------------|");
             Event eve = new Event();
             eve.dataevent("");
             Console.WriteLine(eve.getJenisEvent());
-
-            Console.Write("Press Enter to return to Main Menu");
-            Console.ReadLine();
-
+            Console.ReadKey();
         }
 
         private static bool MainMenu(){
@@ -41,7 +134,7 @@ namespace c_example{
                     ReverseString();
                     return true;
                 case "2":
-                    RemoveWhitespace();
+                    bookEvents();
                     return true;
                 case "3":
                     eventList();
@@ -75,7 +168,7 @@ namespace c_example{
         }
  
         private static void DisplayResult(string message){
-            Console.WriteLine($"\r\nYour modified string is: {message}");
+            Console.WriteLine($"{message}");
             Console.Write("\r\nPress Enter to return to Main Menu");
             Console.ReadLine();
         }
