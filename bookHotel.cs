6a0340b5510ea19077fae65255ef09c9fb0de20d
Ade Mahendra <hendraadem@gmail.com>
2020-07-12 20:53:55 +0700

    upload dari ikhsan

---
+++ bookHotel.cs
@@ -0,0 +1,13 @@
+using System;
+namespace ticketing{
+    public class bookHotel{
+        
+        Console.Write("asdasdasd");
+        asldkjasd
+        asdpookqpdkads
+        as
+        decimalda
+        sda
+
+    }
+}
